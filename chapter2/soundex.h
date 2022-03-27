46db5bebd86049e29944bc510096dc14d8baa91b
m-wuyong <m-wuyong@jiiov.com>
2022-03-27 20:45:14 +0800

    refactor code

--- chapter2/soundex.h
+++ chapter2/soundex.h
@@ -1,18 +1,37 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H 
 #include <string>
+#include <unordered_map>
+
+const size_t MAX_CODE_LENGTH{4};
 
 class Soundex{
 public:
   std::string encode(const std::string& word) const {
-    auto encoded = word.substr(0, 1);
-    if (word.length() > 1) {
-      encoded += "1";
-    }
-    return zero_pad(encoded);
+    return zero_pad(head(word) + encode_digits(word));
+  }
+private:
+  std::string head(const std::string& word) const {
+    return word.substr(0, 1);
+  }
+  std::string encode_digit(char letter) const {
+    const std::unordered_map<char, std::string> encodings{
+      {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+      {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"},
+      {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+      {'d', "3"}, {'t', "3"},
+      {'l', "4"},
+      {'m', "5"}, {'n', "5"},
+      {'r', "6"}
+    };
+    return encodings.find(letter)->second;
+  }
+  std::string encode_digits(const std::string& word) const {
+    if (word.length() > 1) return encode_digit(word[1]);
+    return "";
   }
   std::string zero_pad(const std::string& word) const {
-    int zerosNeeded = 4 - word.length();
+    int zerosNeeded = MAX_CODE_LENGTH - word.length();
     return word + std::string(zerosNeeded, '0');
   }
 };
--- chapter2/soundex_test.cpp
+++ chapter2/soundex_test.cpp
@@ -17,6 +17,9 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
 
 TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
   ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+  ASSERT_THAT(soundex.encode("Ac"), Eq("A200"));
+  ASSERT_THAT(soundex.encode("Ad"), Eq("A300"));
+  ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
 }
 
 
